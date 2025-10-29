e1033e01a3b308c31a89f83e8aeac40cc293fac7
Roy S. <bookingrstubbs@gmail.com>
2025-10-29 23:34:09 +0000

    worked on user model, needs finished

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,25 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    locations: Mapped["LocationsList"] = relationship(
+        back_populates="user", cascade="all, delete-orphan", uselist=False)
+    user_name: Mapped[str] = mapped_column(String, nullable=False)
+    zipcode: Mapped[int] = mapped_column(Integer)
+    # latitude: Mapped[int] =  mapped_column(Integer)
+    # longitude: Mapped[int] = mapped_column(Integer)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "locations": self.locations,
+            "user_name": self.user_name,
+            "zipcode": self.zipcode,
             # do not serialize the password, its a security breach
         }
