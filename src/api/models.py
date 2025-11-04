f3581a5f465cd358c966341369a199ad151b0c07
erivera1993 <erivera1942@gmail.com>
2025-11-04 01:26:57 +0000

    slave drivers

--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean,  JSON
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +18,28 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Location(db.Model):
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)
+    position: Mapped[dict] = mapped_column(JSON, nullable=False)
+    directions: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    # liked_by_users = relationship(
+    #     "User",
+    #     secondary="user_likes",
+    #     back_populates="liked_locations"
+    # )
+
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "name": self.name,
+        "type": self.type,
+        "position": self.position,
+        "directions": self.directions,
+    }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Location
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route("/location", methods=["GET"])
+def get_all_locations():
+    locations = db.session.execute(select(Location)).scalars().all()
+    return jsonify([location.serialize() for location in locations]), 200
+
+
+@api.route('/location', methods=['POST'])
+def create_locations():
+    data = request.get_json()
+    name = data.get("name")
+    type_ = data.get("type")
+    position = data.get("position")
+    directions = data.get("directions")
+
+    if not name or not type_ or not position:
+        return jsonify({"error": "Missing required fields: name, type, or position"}), 400
+
+    new_location = Location(
+        name=name,
+        type=type_,
+        position=position,
+        directions=directions
+    )
+
+    db.session.add(new_location)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Location added",
+        "location": new_location.serialize()
+    }), 201
